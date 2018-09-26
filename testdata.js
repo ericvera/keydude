b13bf200e251f7ad56b85130aaad10cd534ca1a3
Eric Vera <ev@Erics-MacBook.local>
2018-09-26 00:33:51 -0300

    v1

---
+++ testdata.js
@@ -0,0 +1,13 @@
+const TestData = {
+  passphraseIV: 'T0IsmW6JljSCU1jC',
+  wrappedKey: {
+    k: '+2/qhm415pWcFkW6RJe9xM9R+X4Ab1frk6U3Cc3ey8XG9yS9ItAE/N8E+Wrj8qk1',
+    iv: 'sWQi6n2GWAAnpIbr'
+  },
+  encryptedData: {
+    ed:
+      'ur6C1aGocaTZ2yolZZNntHG32YcAXa64DFMQphdB5mTeyJp2vJz51rtjnrlG+EYoiNWgrwHNuH6jEnrFJ89WXiZ1fNA=',
+    iv: 't8cJydeqzglggZwy'
+  },
+  objectToEncrypt: { data: 'hello humans!', date: Date.parse('2018-02-06') }
+}
