51bd0482532409c607fa6d74c917be75724e8fc1
Eric Vera <ev@Erics-MacBook.local>
2018-09-27 10:56:30 -0300

    Cleanup code and documentation

--- README.md
+++ README.md
@@ -8,7 +8,35 @@ Simplified and opinionated crypto library (wraps the Web Crypto API)
 
 # Why keydude?
 
-This cryptography thing is hard to figure out and easy to get wrong. Keydude is optimized for ease of use while. Encryption algorithms and pre-selected for you and so is the output encoding. For more details on the selected options see the 'Opinionated' section at the end.
+This cryptography thing is a pain to figure out and easy to get wrong. The library was built to implement symmetric end-to-end encryption in a web app.
+
+- `SHA-256` for password hashing
+- `AES-GCM` for wrapping and encryption
+- `Base64` as the output
+
+# The basics
+
+DISCLAIMER: This is a high level explanation to get you started and not meant to be a replacement for security training.
+
+In a simplistic way, symmetric encryption is when you use the same key for both encryption and decryption. As opposed to asymmetric encryption where you have a public key that anybody can see to encrypt the data and a private key that you have to keep secret to decrypt the data. Symmetric encryption is better for when you will be both encrypting and decrypting your own data and asymmetric is better when you are sending/receiving the data between different people.
+
+## Key management
+
+For symmetric encryption you need a key. You can generate this key using `keydude.generateEncryptionDecryptionKey()`. You want to keep this key secret. You also want to make it so that if somebody gets access to it they can't just use it to decode your data. For this you 'wrap' the key. This is the equivalent of putting a physical key inside a safe box with password. For this you use `keydude.wrapKey()`.
+
+Wrapping a key requires two things, a passphrase and an initialization vector (IV). The passphrase is easy to understand, you can generate one for each user (you are still able to decode the data if you really wanted to) or you can let the user provide it in the client and not store it anywhere if you want the user data to be completely inaccessible to you. The initialization vector sounds fancy, but it is just an array of cryptographicaly random bytes used to make the encryption more secure.
+
+OK that may require a bit more of explaining. The AES-GCM algorithm is a block-cypher which is fancy speak for 'it encrypts blocks of data a predetermined size at a time'. So if you had repeating blocks encrypted with the same key they would look the same. The initialization vector provided with each encrypting is used by the algorithm to prevent these blocks from being the same.
+
+You can generate the IV using `keydude.generateIV()` which returns a base64 encoded IV. You will have to generate one and store it as you will have to provide it every time you wrap and unwrap your encryption/decryption key.
+
+In summary you, generate a new key for a user with `keydude.generateEncryptionDecryptionKey()`, generate an IV using `keydude.generateIV()`, then wrap that key with `keydude.wrapKey('somepassword', <generated IV>, <generated key>)`. You can store the generated IV and the wrapped key in the database. When you need to use the key to encrypt/decrypt just use `keydude.unwrapKey('somepassword', <generated IV>, <wrapped key>)`. Finally, for convenience, if you are in a trusted client, once the user provides the passphrase and you download and unwrap the key you can re-wrap it and store it locally using some other passphrase so that the user does not have to keep entering the password. This could be using a PIN or some piece of user data like a user id.
+
+## Encryption and decryption
+
+After key management, this part is going to look very easy. Using the key that you extracted from `keydude.unwrapKey()` you can encrypt your data using `keydude.encrypt(<data object>, <unwrapped key>)`. This will return a single base64 encoded string containing both a new initialization vector (you have to generate a new one for every encryption/decryption for the algorithm to be secure) and the encrypted data. You can safely store this in you database or local storage.
+
+Whenever you want to access this information again just call `keydude.decrypt(<encrypted data generated with keydude.encrypt()>, <unwrapped key>)`.
 
 # Usage
 
@@ -18,75 +46,59 @@ This cryptography thing is hard to figure out and easy to get wrong. Keydude is 
 
 `yarn add keydude`
 
-## Use in browser
+## In browser
 
 `<script crossorigin src="https://unpkg.com/keydude@1/dist/keydude.js"></script>`
 
 ## Sample
 
-```javascript
-// Needed is you use through yarn/npm
-const keydude = require('keydude')
+All functions return a Promise. Using async/await here because it is easier to read.
 
+```javascript
+import keydude from 'keydude'
+
+// This should come from the user or something else
+// like a user id (if storing localy) different for every user
 const passphrase = 'my-C0mpl3x-p@ssKe7!$s.'
-```
 
-### Generate a key and wrap it so that it can be stored
+const newKey = await keydude.generateEncryptionDecryptionKey()
 
-```javascript
-// Note: you will need to store both the wrappedKey
-// and the passphraseIV
-keydude.generateEncryptionDecryptionKey().then(key => {
-  keydude.generateIV().then(passphraseIV => {
-    // Store the passphraseIV
-    keydude.wrapKey(passphrase, passphraseIV, key).then(wrappedKey => {
-      // Store this wrapped key
-    })
-  })
-})
-```
+const keyWrappingIV = await keydude.generateIV()
+// sample: 'T0IsmW6JljSCU1jC'
 
-### Unwrap the key so that it can be used for encryption/decryption
+const wrappedKey = await keydude.wrapKey(passphrase, passphraseIV, key)
+// sample: 'oc7/LI1u7tTPGuOZm3oOC20ztbEOTU0Dgp7I5QJPawXGMv44mNqJLIgZ9VNVVgpVbZBUJGpFr7GLJDu5',
 
-```javascript
-// Note: this key is not to be stored without wrapping
-// Both the passphrase and the passphraseIV must be the
-// ones use with wrapKey
-keydude.unwrapKey(passphrase, passphraseIV, wrappedKey).then(unwrappedkey => {
-  // unwrappedKey can now be used to encrypt/decrypt
-})
-```
+// ...store keyWrappingIV and wrappedKey somewhere
+// as you will need to unwrap the key in every
+// session
 
-### Encrypt some data
+const unwrappedKey = await keydude.unwrapKey(
+  passphrase,
+  keyWrappingIV,
+  wrappedKey
+)
 
-```javascript
-keydude
-  .encrypt({ id: 'someid', other: Date.parse('2018-02-06') }, unwrappedKey)
-  .then(encryptedData => {
-    // encryptedData is an object contianing two preperties ed
-    // (encrypted data) and iv (initialization vector). They
-    // are both base64 encrypted.
-  })
-```
+const encryptedData = await keydude.encrypt(
+  { id: 'someid', other: Date.parse('2025-02-06') },
+  unwrappedKey
+)
+// sample: 'VL3RX4U9b55Y6OyTV2/3ifKeNS7/wgnr9ZXiiajADL8bHVu8dyZj9RjzA/Vi4z1M0L0wQ5nV84NHG+FHzrzB9BqghEhpqmzwbQ=='
 
-### Decrypt data
-
-```javascript
-keydude.decrypt(encryptedData, unwrappedKey).then(decryptedObject => {
-  // decryptedObject is the original object that was encrypted.
-})
+const decryptedData = await keydude.decrypt(encryptedData, unwrappedKey)
+// sample: { id: 'someid', other: Date.parse('2025-02-06') }
 ```
 
 # Functions
 
-| Function                                                      | Description                                                                                                                                    |
-| ------------------------------------------------------------- | ---------------------------------------------------------------------------------------------------------------------------------------------- |
-| `generateEncryptionDecryptionKey()`                           | Generates a new encryption/decryption key. You should use wrapKey before storing the key anywhere and then unwrap key when you need to use it. |
-| `generateIV()`                                                | Generate a secure 96-bit initialization vector and returns it as a base64 encoded string.                                                      |
-| `wrapKey(passphrase, base64PassphraseIV, keyToWrap)`          | Wrap (encode) the key using a key generated from the passphrase.                                                                               |
-| `unwrapKey(passphrase, base64PassphraseIV, wrappedKeyObject)` | Unwraps a previously wrapped key so that it can be used.                                                                                       |
-| `encrypt(dataObject, encryptionDecryptionKey)`                | This will JSON.stringify, compress, and finally encrypt the provided object.                                                                   |
-| `decrypt(encryptedDataObject, encryptionDecryptionKey)`       | Call this on the result of an encrypt call in order to decrypt the object.                                                                     |
+| Function                                                      | Description                                                                                                                                   |
+| ------------------------------------------------------------- | --------------------------------------------------------------------------------------------------------------------------------------------- |
+| `generateEncryptionDecryptionKey()`                           | Generates a new encryption/decryption key. You should use wrapKey before storing the key anywhere and then unwrapKey when you need to use it. |
+| `generateIV()`                                                | Generate a secure 96-bit initialization vector and returns it as a base64 encoded string.                                                     |
+| `wrapKey(passphrase, base64PassphraseIV, keyToWrap)`          | Wrap (encode) the key using a key generated from the passphrase.                                                                              |
+| `unwrapKey(passphrase, base64PassphraseIV, wrappedKeyObject)` | Unwraps a previously wrapped key so that it can be used.                                                                                      |
+| `encrypt(dataObject, encryptionDecryptionKey)`                | This will call JSON.stringify, compress, and finally encrypt the provided dataObject.                                                         |
+| `decrypt(encryptedDataObject, encryptionDecryptionKey)`       | Call this on the result of an encrypt call in order to decrypt the object.                                                                    |
 
 # Opinionated
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 const base64js = require('base64-js')
 
-// NOTE: Recomended IV size in 96 bits (find source). In the case of a Uint8Array that is 12 elements.
-const IVSIZE = 12
-// NOTE: Using AES-GCM as it is recommended for security and perf
-// Reference: https://en.wikipedia.org/wiki/Galois/Counter_Mode
-// Reference: https://blog.cryptographyengineering.com/2012/05/19/how-to-choose-authenticated-encryption/
+// See README.md for more detauls on where these numbers and selections come from
 const ALGORITHM = 'AES-GCM'
 const DIGESTALGORITHM = 'SHA-256'
+const IVSIZE = 12
 const LENGTHOFKEY = 256
 
 /**
- * (Internal) Convests the provided string to an Uint8Array
- * @param {String} text Text to convert
+ * (Internal) Converts the provided string to an Uint8Array.
+ * @param {String} text
  * @returns {Uint8Array}
  */
 const toUint8Array = text => {
-  // Original from: http://qnimate.com/passphrase-based-encryption-using-web-cryptography-api/
+  // Adapted from: http://qnimate.com/passphrase-based-encryption-using-web-cryptography-api/
   let array = new Uint8Array(text.length)
   for (let i = 0; i < text.length; i++) {
     array[i] = text.charCodeAt(i)
@@ -25,53 +22,50 @@ const toUint8Array = text => {
 }
 
 /**
- * (Internal) Convests the provided Uint8Array to a string
+ * (Internal) Convests the provided Uint8Array to a string.
  * @param {Uint8Array} uint8Array
  * @returns {String}
  */
 const toString = uint8Array => {
-  var str = ''
-  for (var iii = 0; iii < uint8Array.byteLength; iii++) {
-    str += String.fromCharCode(uint8Array[iii])
+  // Adapted from: http://qnimate.com/passphrase-based-encryption-using-web-cryptography-api/
+  let str = ''
+  for (let i = 0; i < uint8Array.byteLength; i++) {
+    str += String.fromCharCode(uint8Array[i])
   }
 
   return str
 }
 
 /**
- * (Internal) Generate a wrap/unwrap key using the hash of the provided passphrase
- * @param {String} passphrase A text passphrase to hash in order to generate the key
- * @param {String} base64PassphraseIV A base64 initiation vector used to generate the key
+ * (Internal) Generate a wrap/unwrap key using the SHA-256 hash of the provided passphrase.
+ * @param {String} passphrase A text passphrase to hash in order to generate the key.
+ * @param {String} base64PassphraseIV A base64 initiation vector used to generate the key.
  * @returns {Promise<CryptoKey>}
  */
 const generateWrappingKey = async (passphrase, base64PassphraseIV) => {
-  const passphraseBuffer = toUint8Array(passphrase)
-  const passphraseIVByteArray = base64js.toByteArray(base64PassphraseIV)
-  const algorithm = { name: ALGORITHM, iv: passphraseIVByteArray }
+  const passphraseUint8Array = toUint8Array(passphrase)
+  const passphraseIVUint8Array = base64js.toByteArray(base64PassphraseIV)
+  const wrapKeyAlgorithm = { name: ALGORITHM, iv: passphraseIVUint8Array }
 
   const passphraseHash = await crypto.subtle.digest(
     DIGESTALGORITHM,
-    passphraseBuffer
+    passphraseUint8Array
   )
 
-  let wrappingKey
-
-  wrappingKey = await crypto.subtle.importKey(
+  return crypto.subtle.importKey(
     'raw',
     passphraseHash,
-    algorithm,
+    wrapKeyAlgorithm,
     false,
     ['wrapKey', 'unwrapKey']
   )
-
-  return wrappingKey
 }
 
 /**
- * (Internal) Generate a secure 96-bit initialization vector in Uint8Array format
+ * (Internal) Generate a secure 96-bit initialization vector in Uint8Array format.
  * @returns {Promise<Uint8Array>}
  */
-const generateRawIV = async () => {
+const generateIVUint8Array = async () => {
   return Promise.resolve(crypto.getRandomValues(new Uint8Array(IVSIZE)))
 }
 
@@ -81,44 +75,39 @@ const generateRawIV = async () => {
 
 /**
  * Generate a secure 96-bit initialization vector and returns it as a base64 encoded string.
- * @returns {Promise<String>} base64 encoded IV
+ * @returns {Promise<String>} Base64 encoded initialization vector (IV).
  */
 const generateIV = async () => {
-  const iv = await generateRawIV()
+  const ivUint8Array = await generateIVUint8Array()
 
-  return Promise.resolve(base64js.fromByteArray(iv))
+  return Promise.resolve(base64js.fromByteArray(ivUint8Array))
 }
 
 /**
  * Generates a new encryption/decryption key. You should use wrapKey before
- * storing the key anywhere and then unwrap key when you need to use it.
- * @returns {CryptoKey}
+ * storing the key anywhere and then unwrapKey when you need to use it.
+ * @returns {Promise<CryptoKey>}
  */
 const generateEncryptionDecryptionKey = async () => {
-  // Generate a new key for encryption/decryption
-  const newKey = await crypto.subtle.generateKey(
+  return crypto.subtle.generateKey(
     { name: ALGORITHM, length: LENGTHOFKEY },
     true,
     ['encrypt', 'decrypt']
   )
-
-  // Return the generated key
-  return newKey
 }
 
 /**
  * Wrap (encode) the key using a key generated from the passphrase.
- * @param {String} passphrase passphrase used to generate the key used to wrap the provided key
- * @param {String} base64PassphraseIV base64 encoded initiation vector used to generate the wrapping key
- * @param {CryptoKey} keyToWrap key to wrap so that it can be stored
- * @returns {Promise<String>} Base64 encoded String (first 16 characters are the iv, the remaining the encrypted key)
+ * @param {String} passphrase Passphrase used to generate the key used to wrap the provided key.
+ * @param {String} base64PassphraseIV Base64 encoded initialization vector used to generate the wrapping key.
+ * @param {CryptoKey} keyToWrap Key to wrap so that it can be stored.
+ * @returns {Promise<String>} Base64 encoded String (includes both iv and encrypted key data).
  */
 const wrapKey = async (passphrase, base64PassphraseIV, keyToWrap) => {
   const wrappingKey = await generateWrappingKey(passphrase, base64PassphraseIV)
 
-  // Wrap the key
-  const iv = await generateRawIV()
-  const wrappingAlgorithm = { name: ALGORITHM, iv }
+  const newIVUint8Array = await generateIVUint8Array()
+  const wrappingAlgorithm = { name: ALGORITHM, iv: newIVUint8Array }
   const wrappedKey = await crypto.subtle.wrapKey(
     'raw',
     keyToWrap,
@@ -126,40 +115,49 @@ const wrapKey = async (passphrase, base64PassphraseIV, keyToWrap) => {
     wrappingAlgorithm
   )
 
-  // Convert to base64 for easy storage
-  const wrappedKeyArr = new Uint8Array(wrappedKey)
-  const concatenatedArr = new Uint8Array(wrappedKeyArr.length + iv.length)
-  concatenatedArr.set(iv)
-  concatenatedArr.set(wrappedKeyArr, iv.length)
+  // Convert from buffer to Uint8Array
+  const wrappedKeyUint8Array = new Uint8Array(wrappedKey)
+  // Create a single array that fits both the iv and the data
+  const resultUint8Array = new Uint8Array(
+    wrappedKeyUint8Array.length + newIVUint8Array.length
+  )
+  // Set the iv as the first part
+  resultUint8Array.set(newIVUint8Array)
+  // Set the remaining as the encrypted key
+  resultUint8Array.set(wrappedKeyUint8Array, newIVUint8Array.length)
 
-  return base64js.fromByteArray(concatenatedArr)
+  return base64js.fromByteArray(resultUint8Array)
 }
 
 /**
  * Unwraps a previously wrapped key so that it can be used.
- * @param {String} passphrase passphrase used to generate the key used to unwrap the provided wrapped key
- * @param {String} base64PassphraseIV base64 encoded initiation vector used to generate the unwrapping key
- * @param {String} wrappedKey Base64 encoded String (first 16 characters are the iv, the remaining the encrypted key)
- * @returns {CryptoKey}
+ * @param {String} passphrase Passphrase used to generate the key used to unwrap the provided wrapped key.
+ * @param {String} base64PassphraseIV Base64 encoded initialization vector used to generate the unwrapping key.
+ * @param {String} base64WrappedKey Base64 encoded String (includes both iv and encrypted key data).
+ * @returns {Promise<CryptoKey>}
  */
-const unwrapKey = async (passphrase, base64PassphraseIV, wrappedKey) => {
+const unwrapKey = async (passphrase, base64PassphraseIV, base64WrappedKey) => {
   const unwrappingKey = await generateWrappingKey(
     passphrase,
     base64PassphraseIV
   )
 
   // Convert the data from base64 to byte array
-  const rawData = base64js.toByteArray(wrappedKey)
-  const encryptedDataIv = new Uint8Array(IVSIZE)
-  encryptedDataIv.set(rawData.slice(0, IVSIZE))
-  const wrappedKeyContent = new Uint8Array(rawData.length - IVSIZE)
-  wrappedKeyContent.set(rawData.slice(IVSIZE))
+  const wrappedKeyUint8Array = base64js.toByteArray(base64WrappedKey)
+  // Extract first elements which are the IV
+  const wrappedKeyIVUint8Array = new Uint8Array(IVSIZE)
+  wrappedKeyIVUint8Array.set(wrappedKeyUint8Array.slice(0, IVSIZE))
+  // Extract the remaining elements containing the encrypted data
+  const wrappedKeyContentUint8Array = new Uint8Array(
+    wrappedKeyUint8Array.length - IVSIZE
+  )
+  wrappedKeyContentUint8Array.set(wrappedKeyUint8Array.slice(IVSIZE))
 
-  const unwrappingAlgorithm = { name: ALGORITHM, iv: encryptedDataIv }
+  const unwrappingAlgorithm = { name: ALGORITHM, iv: wrappedKeyIVUint8Array }
 
-  const unwrappedKey = await crypto.subtle.unwrapKey(
+  return crypto.subtle.unwrapKey(
     'raw',
-    wrappedKeyContent,
+    wrappedKeyContentUint8Array,
     unwrappingKey,
     unwrappingAlgorithm,
     {
@@ -169,77 +167,66 @@ const unwrapKey = async (passphrase, base64PassphraseIV, wrappedKey) => {
     false,
     ['encrypt', 'decrypt']
   )
-
-  // Return the wrapped key
-  return unwrappedKey
 }
 
 /**
- * This will JSON.stringify, compress, and finally encrypt the provided object.
- * @param {Object} dataObject Object to be encrypted
- * @param {CryptoKey} encryptionDecryptionKey Key used to encrypt the object
- * @returns {Promise<String>} Base64 encoded String. The first 16 characters are the iv. The remaining the data.
+ * This will call JSON.stringify, compress, and finally encrypt the provided dataObject.
+ * @param {Object} dataObject Object to be encrypted.
+ * @param {CryptoKey} encryptionDecryptionKey Key used to encrypt the object.
+ * @returns {Promise<String>} Base64 encoded String (includes both iv and encrypted key data).
  */
 const encrypt = async (dataObject, encryptionDecryptionKey) => {
-  // Source : https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/encrypt
+  const newIVUint8Array = await generateIVUint8Array()
+  const dataToEncryptUint8Array = toUint8Array(JSON.stringify(dataObject))
+  const algorithm = { name: ALGORITHM, iv: newIVUint8Array }
 
-  // Generate a new IV
-  const iv = await generateRawIV()
-
-  // Stringify and compressthe dataObject
-  const arrayBufferToEncrypt = toUint8Array(JSON.stringify(dataObject))
-
-  // Define algorithm
-  // NOTE: Use AES-GCM based on recommendations at https://en.wikipedia.org/wiki/Galois/Counter_Mode
-  const algorithm = { name: ALGORITHM, iv }
-
-  // Encrypt
-  const dataBuffer = await crypto.subtle.encrypt(
+  const encryptedDataArrayBuffer = await crypto.subtle.encrypt(
     algorithm,
     encryptionDecryptionKey,
-    arrayBufferToEncrypt
+    dataToEncryptUint8Array
   )
 
-  // Convert to base64 for easy storage
-  const dataArr = new Uint8Array(dataBuffer)
-  const concatenatedArr = new Uint8Array(dataArr.length + iv.length)
-  concatenatedArr.set(iv)
-  concatenatedArr.set(dataArr, iv.length)
+  // Convert ArrayBuffer to Uint8Array
+  const encryptedDataUint8Array = new Uint8Array(encryptedDataArrayBuffer)
+  // Create a single array that will contain both iv and encrypted data
+  const resultUint8Array = new Uint8Array(
+    encryptedDataUint8Array.length + newIVUint8Array.length
+  )
+  resultUint8Array.set(newIVUint8Array)
+  resultUint8Array.set(encryptedDataUint8Array, newIVUint8Array.length)
 
-  return base64js.fromByteArray(concatenatedArr)
+  return base64js.fromByteArray(resultUint8Array)
 }
 
 /**
  * Call this on the result of an encrypt call in order to decrypt the object.
- * @param {String} encryptedData Base64 encoded String (first 16 characters are the iv, the remaining the data)
- * @param {CryptoKey} encryptionDecryptionKey Key used to decrypt the object
- * @returns {Object} Decrypted object (decompressed and JSON.parse called to reverse encrypt process)
+ * @param {String} base64EncryptedData Base64 encoded String (contains both the iv and the encrypted data).
+ * @param {CryptoKey} encryptionDecryptionKey Key used to decrypt the object.
+ * @returns {Promise<Object>} Decrypted object (decompressed and JSON.parse called to reverse encrypt process).
  */
-const decrypt = async (encryptedData, encryptionDecryptionKey) => {
-  // Source: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/decrypt
-
+const decrypt = async (base64EncryptedData, encryptionDecryptionKey) => {
   // Convert the data from base64 to byte array
-  const rawData = base64js.toByteArray(encryptedData)
-  const encryptedDataIv = new Uint8Array(IVSIZE)
-  encryptedDataIv.set(rawData.slice(0, IVSIZE))
-  const encryptedDataContent = new Uint8Array(rawData.length - IVSIZE)
-  encryptedDataContent.set(rawData.slice(IVSIZE))
+  const encryptedDataUint8Array = base64js.toByteArray(base64EncryptedData)
+  // Extract iv and encrypted data from the combined array
+  const encryptedDataIVUint8Array = new Uint8Array(IVSIZE)
+  encryptedDataIVUint8Array.set(encryptedDataUint8Array.slice(0, IVSIZE))
+  const encryptedDataContentUint8Array = new Uint8Array(
+    encryptedDataUint8Array.length - IVSIZE
+  )
+  encryptedDataContentUint8Array.set(encryptedDataUint8Array.slice(IVSIZE))
 
-  // Define algorithm
-  // NOTE: User AES-GCM based on recommendations at https://en.wikipedia.org/wiki/Galois/Counter_Mode
-  const algorithm = { name: ALGORITHM, iv: encryptedDataIv }
+  const algorithm = { name: ALGORITHM, iv: encryptedDataIVUint8Array }
 
-  // Decrypt
-  const decryptedData = await crypto.subtle.decrypt(
+  const decryptedDataArrayBuffer = await crypto.subtle.decrypt(
     algorithm,
     encryptionDecryptionKey,
-    encryptedDataContent
+    encryptedDataContentUint8Array
   )
 
-  // Convert to string
-  const decryptedStringifiedObject = toString(new Uint8Array(decryptedData))
+  const decryptedStringifiedObject = toString(
+    new Uint8Array(decryptedDataArrayBuffer)
+  )
 
-  // Convert from stringified back to JSON object
   return JSON.parse(decryptedStringifiedObject)
 }
 
